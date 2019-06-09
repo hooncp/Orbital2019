af428f85a1efe43e3bac41343f7aeb14e101ae07
hooncp <hooncheep@gmail.com>
2019-06-09 17:37:27 +0800

    added sliding activity for first time users

--- app/src/main/java/com/example/islandmark/SplashActivity.java
+++ app/src/main/java/com/example/islandmark/SplashActivity.java
@@ -10,7 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        Intent intent = new Intent(SplashActivity.this, SlidingActivity.class);
         startActivity(intent);
         finish();
     }
