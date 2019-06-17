7ccc6a1ef7deb208d87a6a3ec5c1442c58bea60e
naixe <ahuanggx@gmail.com>
2019-06-17 18:23:53 +0800

    renamed file

--- app/src/main/java/com/example/islandmark/Ar_fragment.java
+++ app/src/main/java/com/example/islandmark/Ar_fragment.java
@@ -14,6 +14,7 @@ import com.google.ar.sceneform.ux.ArFragment;
 
 public class Ar_fragment extends Fragment {
 
+
     private ArFragment arFragment;
     private AccountFragment.OnFragmentInteractionListener mListener;
 
